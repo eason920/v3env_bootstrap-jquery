f8f4171e3a225cdf5199e6cbecb3139ed2f476bd
eason.nb <eason.nb@gmail.com>
2021-11-01 22:17:03 +0800

    start

---
+++ src/views/Modal.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <router-link to="/modal/">Normal Modal</router-link> | 
+    <router-link to="/modal/withjq">Modal with JQuery</router-link>
+    <router-view></router-view>
+  </div>
+</template>
